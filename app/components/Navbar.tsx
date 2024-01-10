f5129d62042c6a0f4cfc16a0bc345fc9ef3240b7
otgon123 <220054eo@yse-c.net>
2024-01-10 12:11:36 +0900

    update

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+
+const Navbar = () => {
+    return(
+        <nav>
+            <div className="flex flex-wrap items-center mx-auto py-4">
+                <Link href="/" className="ms-3 me-3">
+                    <span className="self-center text-2xl font-semibold">Next tweet</span>
+                </Link>
+                <div>
+                    <ul className="flex flex-wrap">
+                        <li>
+                            <Link href="/user/profile" className="py-2 px-3">Profile</Link>
+                        </li>
+                        <li>
+                            <Link href="/regist" className="py-2 px-3">Register</Link>
+                        </li>
+                        <li>
+                            <Link href="/login" className="py-2 px-3">sign in</Link>
+                        </li>
+                        <li>
+                            <Link href="/user/logout" className="py-2 px-3">Sign out</Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+}
+export default Navbar;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
+import Navbar from '@/app/components/Navbar'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Next tweet',
@@ -16,7 +15,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+      <body>
+        <Navbar />
+        <main className='flex min-h-screen flex-col p-5'>
+          {children}
+        </main>
+      </body>
+    </html >
   )
 }
