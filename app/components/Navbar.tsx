96b9f5dbabbfb0f943aaaeb53e7e85873535ed16
otgon123 <220054eo@yse-c.net>
2024-01-19 09:19:27 +0900

    update

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,30 +1,43 @@
-import Link from "next/link";
+"use client"
+
+import Link from 'next/link';
+import { SiLoopback } from 'react-icons/si';
+import NavbarLink from './NavbarLink';
 
 const Navbar = () => {
-    return(
+    console.log('Navbar!!!')
+    return (
         <nav>
-            <div className="flex flex-wrap items-center mx-auto py-4">
-                <Link href="/" className="ms-3 me-3">
-                    <span className="self-center text-2xl font-semibold">Next tweet</span>
-                </Link>
+            <div className="flex px-5 py-3 border-b">
+                <div className="flex items-center mr-6">
+                    <SiLoopback className="me-3" size="2em" />
+                    <Link href="/" className="ms-3 me-3">
+                        <span className="self-center text-black text-2xl font-semibold">Next Tweet</span>
+                    </Link>
+                </div>
+
                 <div>
-                    <ul className="flex flex-wrap">
-                        <li>
-                            <Link href="/user/profile" className="py-2 px-3">Profile</Link>
-                        </li>
-                        <li>
-                            <Link href="/regist" className="py-2 px-3">Register</Link>
-                        </li>
-                        <li>
-                            <Link href="/login" className="py-2 px-3">sign in</Link>
-                        </li>
-                        <li>
-                            <Link href="/user/logout" className="py-2 px-3">Sign out</Link>
-                        </li>
-                    </ul>
+                    <NavbarLink
+                        href="/user/profile"
+                        label="Profile"
+                    />
+                    <NavbarLink
+                        href="/auth/regist"
+                        label="Register"
+                    />
+                    <NavbarLink
+                        href="/auth/login"
+                        label="Sign in"
+                    />
+                    <NavbarLink
+                        href="#"
+                        label="Sign out"
+                        onClick={() => { alert('Sign out!!!!') }}
+                    />
                 </div>
             </div>
         </nav>
     );
 }
+
 export default Navbar;
