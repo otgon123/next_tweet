c6051b60a1125b279800612aea5a3726e49eb498
otgon123 <220054eo@yse-c.net>
2024-01-24 12:02:50 +0900

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,39 @@
+"use client"
+
+import { useEffect, useState } from "react";
+import { User, testUser } from "./models/User";
+import { Tweet } from "./models/Tweet";
+import { getTweets } from "./services/TweetService";
+import TweetForm from "./components/tweet/TweetForm";
+import TweetList from "./components/tweet/TweetList";
 
 export default function Home() {
+  //テストユーザの取得
+  const [user] = useState<User>(testUser);
+  const [tweets, setTweets] = useState<Tweet[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      if (user?.accessToken) {
+        //APIからTweetデータ取得
+        const data = await getTweets(user.accessToken);
+        console.log(data)
+        //データ設定
+        setTweets(data);
+      }
+    })();
+  }, [user])
+
   return (
     <div>
-      <textarea className="resize-none w-full h-24 border rounded-md p-2"></textarea>
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg">Send</button>
+      {
+        user?.id > 0 && (
+          <>
+            <TweetForm />
+            <TweetList tweets={tweets} />
+          </>
+        )
+      }
     </div>
   )
 }
